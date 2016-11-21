3989a94f1884ce9e65e53df15eea375d1bacae23
郑俊鑫 <zhengjunxin@wepiao.com>
2016-11-21 14:16:14 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {Router, Route, hashHistory} from 'react-router'
+import {Provider} from 'react-redux'
+import configureStore from './app/configureStore'
+
+const store = configureStore()
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path="/" component={App}></Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
