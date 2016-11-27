5ebd5730ddde54ab9bdc41c98688c723a3ccc9c8
郑俊鑫 <zhengjunxin@wepiao.com>
2016-11-27 15:45:16 +0800

    boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './components/App'
 import {Router, Route, hashHistory} from 'react-router'
 import {Provider} from 'react-redux'
 import configureStore from './app/configureStore'
